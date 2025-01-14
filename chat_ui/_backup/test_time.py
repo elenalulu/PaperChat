01711f41c6cf0b360e184f8a0a129957c0db4b4e
elenalulu <34589890+elenalulu@users.noreply.github.com>
2025-01-14 11:02:05 +0800

    updated

--- chat_ui/_backup/test_time.py
+++
@@ -1,7 +0,0 @@
-import time
-
-# 获取当前时间的时间戳
-current_timestamp = time.time()
-current_timestamp = str(current_timestamp).split('.')[0]
-
-print(current_timestamp)
--- chat_ui/_backup/test_url.py
+++
@@ -1,6 +0,0 @@
-from urllib.request import urlopen
-
-url = 'https://pdf.dfcfw.com/pdf/H3_AP202412191641354785_1.pdf'
-html = urlopen(url)
-output = html.read().decode("utf-8")
-print (output)
